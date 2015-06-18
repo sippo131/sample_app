b6f46d152be5ff1e02e850b34240f340c07bf27c
sippo131 <sippo_131@yahoo.co.jp>
2015-06-18 22:46:45 +0900

    before sample users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index,:edit, :update]
+  before_action :correct_user, only: [:edit,:update]
+
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  # 本来はユーザーページへのuser_urlやけど、@userでok
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 
   def create
    @user = User.new(user_params)
@@ -25,6 +45,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 
 end
